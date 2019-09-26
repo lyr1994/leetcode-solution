8e14da95560b52557b9c0dba2bdd4338c39b5a37
lyr1994 <lyray1994@gmail.com>
2019-09-25 23:36:33 -0700

    230-kth-smallest-element-in-a-bst

---
+++ medium/230-kth-smallest-element-in-a-bst.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+        for (int i = 0; i < k - 1; i++) {
+            TreeNode node = stack.pop();
+            if (node.right != null) {
+                node = node.right;
+                while (node != null) {
+                    stack.push(node);
+                    node = node.left;
+                }
+            }
+        }
+        return stack.peek().val;
+    }
+}
